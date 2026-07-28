ef792413a9687edede75a6cda649e9d806f2de88
Francisco Martín Vélez Manrique <francisco446742@gmail.com>
2026-07-28 09:13:49 +0200

    added vial

--- quantum/via.c
+++ quantum/via.c
@@ -28,6 +28,10 @@
 
 #include "via.h"
 
+#ifdef VIAL_ENABLE
+#    include "vial.h"
+#endif
+
 #include "raw_hid.h"
 #include "dynamic_keymap.h"
 #include "eeconfig.h"
@@ -473,6 +477,12 @@ void raw_hid_receive(uint8_t *data, uint8_t length) {
         }
         case id_dynamic_keymap_set_encoder: {
             dynamic_keymap_set_encoder(command_data[0], command_data[1], command_data[2] != 0, (command_data[3] << 8) | command_data[4]);
+            break;
+        }
+#endif
+#ifdef VIAL_ENABLE
+        case 0xFE: {
+            vial_handle_cmd(data, length);
             break;
         }
 #endif
